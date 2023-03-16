1151a9f23eda89471f00ff49f7f9cc66407b8825
Rizky Fahrureza <108663566+Mzzqq@users.noreply.github.com>
2023-03-16 19:28:03 +0700

    Update tugassmt2.cpp

--- tugassmt2.cpp
+++ tugassmt2.cpp
@@ -2,43 +2,44 @@
 
 using namespace std;
 
-int compress(char* karakter, int n) {
-    int i = 0;
-    int j = 0;
-    int count;
+int compress(char* chars, int n) {
+    int i = 0; // pointer untuk menunjuk karakter yang sedang diproses
+    int j = 0; // pointer untuk menunjuk posisi hasil kompresi yang akan ditulis
+    int count; // jumlah kemunculan karakter yang sedang diproses
 
     while (i < n) {
-        karakter[j++] = karakter[i++];
-        count = 1;
-        while (i < n && karakter[i] == karakter[i-1]) {
-            i++;
+        chars[j] = chars[i]; // tulis karakter yang sedang diproses pada posisi hasil kompresi
+        count = 1; // reset jumlah kemunculan karakter
+        while (i + 1 < n && chars[i] == chars[i+1]) { // hitung kemunculan karakter
             count++;
+            i++;
         }
-        if (count > 1) {
+        if (count > 1) { // jika kemunculan karakter lebih dari 1, tambahkan angka setelah karakter
             string count_str = to_string(count);
-            for (char c : count_str) {
-                karakter[j++] = c;
+            for (int k = 0; k < count_str.length(); k++) {
+                chars[++j] = count_str[k];
             }
         }
+        i++;
+        j++;
     }
 
-    return j;
+    return j; // kembalikan panjang array hasil kompresi
 }
 
 int main() {
-    char karakter[] = {'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c'};
-    int n = sizeof(karakter) / sizeof(karakter[0]);
+    char chars[] = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    int n = sizeof(chars) / sizeof(chars[0]);
 
-    int len = compress(karakter, n);
+    int len = compress(chars, n);
 
     cout << "Output: " << len << ", [";
     for (int i = 0; i < len; i++) {
-        cout << "\"" << karakter[i] << "\"";
+        cout << "\"" << chars[i] << "\"";
         if (i < len-1) {
             cout << ",";
         }
     }
-
     cout << "]" << endl;
 
     return 0;
