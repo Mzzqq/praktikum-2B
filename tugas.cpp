0d76aeac3c4485b025752b1fac3ebd94672cfa52
Rizky Fahrureza <108663566+Mzzqq@users.noreply.github.com>
2023-04-21 16:36:49 +0700

    Update tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -18,17 +18,34 @@ void push(int data)
     temp->data = data;
     temp->next = top;   // menambahkan data ke atas tumpukan
     top = temp;
+    cout << "Menambahkan " << data << " ke atas tumpukan. Tumpukan saat ini: ";
+    node *curr = top;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
 }
 
 void pop()
 {
     node *temp = top;   // menyimpan data paling atas tumpukan
     top = top->next;    // menghapus data paling atas dari tumpukan
+    cout << "Menghapus " << temp->data << " dari atas tumpukan. Tumpukan saat ini: ";
     delete temp;        // menghapus data dari memori
+    node *curr = top;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
 }
 
-void display()
+/*void display()
 {
+    cout << "Tumpukan saat ini: ";
     node *temp = top;   // menyimpan data paling atas tumpukan
     while (temp != NULL)
     {
@@ -36,17 +53,19 @@ void display()
         temp = temp->next;
     }
     cout << endl;
-}
+}*/
 
 void hanoi(int n, char from, char to, char aux)
 {
     if (n == 1)
     {
-        cout << "Pindahkan batu " << n * 10 << " dari " << from << " ke " << to << endl;   // kasus dasar untuk satu batu
+        cout << "Memindahkan batu " << n * 10 << " dari " << from << " ke " << to << endl;   // kasus dasar untuk satu batu
+        pop();
         return;
     }
     hanoi(n - 1, from, aux, to);    // pindahkan n-1 batu dari 'from' ke 'aux', menggunakan 'to' sebagai bantuan
-    cout << "Pindahkan batu " << n * 10 << " dari " << from << " ke " << to << endl;   // pindahkan batu n dari 'from' ke 'to'
+    cout << "Memindahkan batu " << n * 10 << " dari " << from << " ke " << to << endl;   // pindahkan batu n dari 'from' ke 'to'
+    pop();
     hanoi(n - 1, aux, to, from);    // pindahkan n-1 batu dari 'aux' ke 'to', menggunakan 'from' sebagai bantuan
 }
 
@@ -55,9 +74,10 @@ int main()
     int n = 3; // jumlah batu
     for (int i = n; i >= 1; i--)
     {
-        push(i);    // menambahkan batu ke tumpukan
+        push(i*10);    // menambahkan batu ke tumpukan
     }
-    cout << "Menara Hanoi dengan " << n << " batu" << endl;
+    //display();
+    cout << "\nMenara Hanoi dengan " << n << " batu" << endl;
     hanoi(n, 'A', 'C', 'B');   // memindahkan batu-batu dari tiang A ke tiang C, menggunakan tiang B sebagai bantuan
     getch();
     return 0;
