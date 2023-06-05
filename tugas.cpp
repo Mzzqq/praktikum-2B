4156a72268ee5fef6f9019554e0e1cc28410d8ac
Rizky Fahrureza <108663566+Mzzqq@users.noreply.github.com>
2023-06-05 14:33:55 +0700

    Update tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -1,46 +1,79 @@
 #include <iostream>
+#include <climits>
 
-using namespace std;
+#define V 4 // Jumlah simpul
 
-int compress(char* chars, int n) {
-    int i = 0; // pointer untuk menunjuk karakter yang sedang diproses
-    int j = 0; // pointer untuk menunjuk posisi hasil kompresi yang akan ditulis
-    int count; // jumlah kemunculan karakter yang sedang diproses
+/*
+    Note:
+    Untuk menghindari conflict name pada global, disini tidak mendeklarasikan
+    using namespace std;. sebagai gantinya di dalam code ini mendeklarasikan
+    std:: secara eksplisit
+*/
 
-    while (i < n) {
-        chars[j] = chars[i]; // tulis karakter yang sedang diproses pada posisi hasil kompresi
-        count = 1; // reset jumlah kemunculan karakter
-        while (i + 1 < n && chars[i] == chars[i+1]) { // hitung kemunculan karakter
-            count++;
-            i++;
+int tsp(int graph[V][V], int path[], int curr, int mask){
+    if (mask == (1 << V) - 1) // Jika semua simpul telah dikunjungi
+        return graph[curr][0]; // Kembali ke simpul awal
+
+    int ans = INT_MAX;
+    for (int i = 0; i < V; i++){
+        if ((mask & (1 << i)) == 0){ // Periksa simpul yang belum dikunjungi
+            int new_mask = mask | (1 << i); // Tandai simmpul baru sebagai dikunjungi
+            int new_ans = graph[curr][i] + tsp(graph, path, i, new_mask); // Recursion ke simpul berikutnya
+            ans = std::min(ans, new_ans);
         }
-        if (count > 1) { // jika kemunculan karakter lebih dari 1, tambahkan angka setelah karakter
-            string count_str = to_string(count);
-            for (int k = 0; k < count_str.length(); k++) {
-                chars[++j] = count_str[k];
-            }
-        }
-        i++;
-        j++;
     }
 
-    return j; // kembalikan panjang array hasil kompresi
+    return ans;
 }
 
-int main() {
-    char chars[] = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
-    int n = sizeof(chars) / sizeof(chars[0]);
+void printPath(int path[]){
+    for (int i = 0; i < V; i++)
+        std::cout << path[i] + 1 << " -> ";
+    std::cout << path[0] + 1 << std::endl;
+}
 
-    int len = compress(chars, n);
-
-    cout << "Output: " << len << ", [";
-    for (int i = 0; i < len; i++) {
-        cout << "\"" << chars[i] << "\"";
-        if (i < len-1) {
-            cout << ",";
-        }
+void printGraph(int graph[V][V]){
+    std::cout << "Graph:" << std::endl;
+    for (int i = 0; i < V; i++){
+        for (int j = 0; j < V; j++)
+            std::cout << graph[i][j] << " ";
+        std::cout << std::endl;
     }
-    cout << "]" << endl;
+}
+
+int main(){
+    int graph[V][V] = {{0, 4, 1, 3},    // Matrix cost route antar simpul
+                       {4, 0, 2, 1},
+                       {1, 2, 0, 5},
+                       {3, 1, 5, 0}};
+
+    int start_node;
+    std::cout << "Masukkan titik awal (1-" << V << "): ";
+    std::cin >> start_node;
+
+    if (start_node < 1 || start_node > V){
+        std::cout << "Titik awal tidak valid.\n";
+        return 0;
+    }
+
+    int path[V];
+    for (int i = 0; i < V; i++)
+        path[i] = i;
+
+    // Geser titik awal berdasarkan input
+    int geser = start_node - 1;
+    for (int i = 0; i < V; i++)
+        path[i] = (path[i] + geser) % V;
+
+    //std::cout << "Input Graph:" << std::endl;
+    printGraph(graph);
+
+    int min_cost = tsp(graph, path, 0, 1);
+
+    std::cout << "Jalur terpendek: ";
+    printPath(path);
+
+    std::cout << "Minimum cost: " << min_cost << std::endl;
 
     return 0;
 }
